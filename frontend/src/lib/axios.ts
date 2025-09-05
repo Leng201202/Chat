f4b39bdf910ce7e7752efbbf4fb154020f7cd5aa
Leng <6631503129@lamduan.mfu.ac.th>
2025-09-05 14:05:29 +0700

    cors

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://chatkie.onrender.com/api",
   withCredentials: true, // if using cookie auth
   headers: {
     "Content-Type": "application/json",
