ea9dfd16a1f53ee30762a02ca31adfd273cb4498
Leng <6631503129@lamduan.mfu.ac.th>
2025-09-05 13:46:17 +0700

    Frontend deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy:{
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://chatkie.onrender.com',
         changeOrigin:true,
       }
     }
